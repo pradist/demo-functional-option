c8a8b7aa75fb8eb40222f6257c2c17a7bc7a9628
Pradist Keawborisut <pradist.kea@ascendcorp.com>
2022-10-31 20:31:25 +0700

    feat: extract function

--- main.go
+++ main.go
@@ -7,18 +7,24 @@ import (
 	"time"
 )
 
+func NewServer() http.Server {
+	s := http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	return s
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello World")
 	})
 
-	s := http.Server{
-		Addr:         ":8080",
-		Handler:      mux,
-		ReadTimeout:  1 * time.Second,
-		WriteTimeout: 1 * time.Second,
-	}
+	s := NewServer()
+	s.Handler = mux
 
 	log.Println("Server started", s.Addr)
 	if err := s.ListenAndServe(); err != nil {
